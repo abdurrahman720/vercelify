d33139671c7b79fc48b3bd94be7e9480fdf98ee0
Abdur Rahman <personal.abdurrahman@gmail.com>
2024-02-18 19:00:09 +0600

    build server logic

---
+++ build-server/script.js
@@ -0,0 +1,61 @@
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const mime = require("mime-types");
+
+const s3Client = new S3Client({
+  region: "ap-south-1",
+  credentials: {
+    accessKeyId: "AKIAVRUVSYXL4P62ZAGJ",
+    secretAccessKey: "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",
+  },
+});
+
+const PROJECT_ID = process.env.PROJECT_ID;
+
+async function init() {
+  console.log("executing script...");
+
+  const outDirPath = path.join(__dirname, "output");
+
+  const p = exec(`cd ${outDirPath} && npm install && npm run build`);
+
+  p.stdout.on("data", function (data) {
+    console.log(data.toString());
+  });
+
+  p.stdout.on("error", function (data) {
+    console.log("ERROR", data.toString());
+  });
+
+  p.on("close", async function () {
+    console.log("Build Completed");
+    const distFolderPath = path.join(__dirname, "output/dist");
+    const distFolderContents = fs.readdirSync(distFolderPath, {
+      recursive: true,
+    }); // will get every folder under folder
+
+    for (const filePath of distFolderContents) {
+      if (fs.lstatSync(filePath).isDirectory()) {
+        continue;
+      }
+        
+        console.log("uploading file path")
+      //else read the files and upload on s3
+      const command = new PutObjectCommand({
+        Bucket: "vercelify",
+        Key: `__outputs/${PROJECT_ID}/${filePath}`,
+        Body: fs.createReadStream(filePath),
+        ContentType: mime.lookup(filePath),
+      });
+
+        await s3Client.send(command);
+        console.log("Uploaded,", filePath)
+    }
+
+    console.log("done...");
+  });
+}
+
+init();
